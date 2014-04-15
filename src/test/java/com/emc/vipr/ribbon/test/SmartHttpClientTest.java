4ffd9c81e1b8ed43507d88fff82723ceae08de5f
Stu Arnett <christopher.arnett@emc.com>
2014-04-15 16:43:14 -0500

    Removed System.out output from SmartHttpClientTests and made vipr.s3.endpoints optional in S3 smart client tests.

--- src/test/java/com/emc/vipr/ribbon/test/SmartHttpClientTest.java
+++ src/test/java/com/emc/vipr/ribbon/test/SmartHttpClientTest.java
@@ -25,26 +25,37 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class SmartHttpClientTest {
     @Test
     public void testRibbonClient() throws Exception {
+        int totalRequests = 20;
+        String vipAddress = "www.foo.com";
+        String serverString = "www.yahoo.com:80,www.google.com:80,www.bing.com:80,www.icann.org:80,www.linkedin.com:80";
+        List<Server> servers = SmartClientConfig.parseServerList(serverString);
+
         String clientName = "testRibbonClient";
         SmartHttpClient client = new SmartHttpClient(clientName,
-                new SmartClientConfig().withInitialNodes("www.yahoo.com:80,www.google.com:80,www.bing.com:80,www.icann.org:80,www.linkedin.com:80")
-                        .withVipAddresses("www.foo.com")
+                new SmartClientConfig().withInitialNodes(servers.toArray(new Server[servers.size()])).withVipAddresses(vipAddress)
         );
-        // fire off 20 requests
-        for (int i = 0; i < 20; i++) {
-            HttpEntity e = client.execute(new HttpGet("http://www.foo.com/")).getEntity();
+
+        // fire off a number of requests
+        for (int i = 0; i < totalRequests; i++) {
+            HttpEntity e = client.execute(new HttpGet("http://" + vipAddress + "/")).getEntity();
             if (e != null) readAndClose(e.getContent());
         }
         BaseLoadBalancer lb = (BaseLoadBalancer) client.getLoadBalancer();
         LoadBalancerStats lbStats = lb.getLoadBalancerStats();
-        assertNull("No requests should go to www.foo.com", lbStats.getServerStats().get(new Server("www.foo.com", 80)));
-        System.out.println(lbStats);
+        assertNull("No requests should go to " + vipAddress, lbStats.getServerStats().get(new Server(vipAddress, 80)));
+        int requestCount = 0;
+        for (Server server : servers) {
+            requestCount += lbStats.getSingleServerStat(server).getTotalRequestsCount();
+            assertTrue("At least one request should go to " + server, lbStats.getSingleServerStat(server).getTotalRequestsCount() > 0);
+        }
+        assertEquals("Total requests should be " + totalRequests, totalRequests, requestCount);
     }
 
     private void readAndClose(InputStream is) throws IOException {
